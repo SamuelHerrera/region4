8a07d6254545fc0cbbc3f6cf4bc5f692eeb0bb2d
Samuel Herrera <samuelherrerafuente@gmail.com>
2018-04-29 20:14:35 -0500

    ladingPage

--- src/app/components/reporte/reporte.component.ts
+++ src/app/components/reporte/reporte.component.ts
@@ -82,7 +82,7 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
         this.urlAux += "&markers=color:blue%7Clabel:" + idx + "%7C" + element.latitud + "," + element.longitud;
         idx++;
       });
-      this.urlAux += "&markers=color:green%7Clabel:*%7C"+this.lat +","+this.lng+"&";
+      this.urlAux += "&markers=color:green%7Clabel:*%7C" + this.lat + "," + this.lng + "&";
       this.yals.urlToBase64("https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=700x400&format=jpg&maptype=roadmap"
         + this.urlAux +
         "key=AIzaSyDV1v9VqdOKgnwilfhA25PqEFRoSNjHXAQ")
@@ -174,14 +174,14 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
         datasets: [
           {
             label: 'Precio Promedio Nuevo',
-            backgroundColor: '#0000FF',
-            borderColor: '#0080FF',
+            backgroundColor: '#0d48a8',
+            borderColor: '#082759',
             data: coloniaGeneralNuevo
           },
           {
             label: 'Precio Promedio Usado',
-            backgroundColor: '#2E64FE',
-            borderColor: '#0080FF',
+            backgroundColor: '#0d52c1',
+            borderColor: '#0341a5',
             //data: this.datos.data.response.colonia_preciosm2_general.data.usado
             data: this.coloniam2General
           }
@@ -208,14 +208,14 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
         datasets: [
           {
             label: 'Nuevo',
-            backgroundColor: '#82FA58',
-            borderColor: '#74DF00',
+            backgroundColor: '#0d48a8',
+            borderColor: '#082759',
             //data: this.datos.data.response.colonia_preciosm2_especifica.data.nuevo
             data: coloniaEspecificaNuevo
           }, {
             label: 'Usado',
-            backgroundColor: '#C8FE2E',
-            borderColor: '#74DF00',
+            backgroundColor: '#0d52c1',
+            borderColor: '#0341a5',
             //data: this.datos.data.response.colonia_preciosm2_especifica.data.usado
             data: coloniaEspecificaUsado
           }
@@ -272,14 +272,14 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
         datasets: [
           {
             label: 'Nuevo',
-            backgroundColor: '#82FA58',
-            borderColor: '#74DF00',
+            backgroundColor: '#0d48a8',
+            borderColor: '#082759',
             //data: this.datos.data.response.colonia_tipos_propiedades.data.nuevo
             data: tipoColoniaNuevo
           }, {
             label: 'Usado',
-            backgroundColor: '#C8FE2E',
-            borderColor: '#74DF00',
+            backgroundColor: '#0d52c1',
+            borderColor: '#0341a5',
             //data: this.datos.data.response.colonia_tipos_propiedades.data.usado
             data: tipoColoniaUsado
           }
@@ -310,14 +310,14 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
           datasets: [
             {
               label: 'Nuevo',
-              backgroundColor: '#82FA58',
-              borderColor: '#74DF00',
+              backgroundColor: '#0d48a8',
+              borderColor: '#082759',
               //data: this.datos.data.response.colonia_recamaras ? this.datos.data.response.colonia_recamaras.data.nuevo : []
               data: recamarasNuevo
             }, {
               label: 'Usado',
-              backgroundColor: '#C8FE2E',
-              borderColor: '#74DF00',
+              backgroundColor: '#0d52c1',
+              borderColor: '#0341a5',
               //data: this.datos.data.response.colonia_recamaras ? this.datos.data.response.colonia_recamaras.data.usado : []
               data: recamarasUsado
             }
@@ -345,15 +345,15 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
           datasets: [
             {
               label: 'Nuevo',
-              backgroundColor: '#82FA58',
-              borderColor: '#74DF00',
+              backgroundColor: '#0d48a8',
+              borderColor: '#082759',
               //data: this.datos.data.response.colonia_superficies ? this.datos.data.response.colonia_superficies.data.nuevo : []
               data: coloniaNuevo
             },
             {
               label: 'Usado',
-              backgroundColor: '#C8FE2E',
-              borderColor: '#74DF00',
+              backgroundColor: '#0d52c1',
+              borderColor: '#0341a5',
               //data: this.datos.data.response.colonia_superficies ? this.datos.data.response.colonia_superficies.data.usado : []
               data: coloniaUsado
             }
@@ -380,8 +380,8 @@ export class ReporteComponent implements OnInit, OnChanges, AfterViewInit {
             label: 'Precio Promedio por m2',
             data: precioHistorico,
             fill: false,
-            backgroundColor: '#4bc0c0',
-            borderColor: '#4bc0c0'
+            backgroundColor: '#0e63ed',
+            borderColor: '#054dc1'
           }
         ]
       }
