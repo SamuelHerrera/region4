e1abc6c89a510010cd9221e3dcd4f1806fe5117b
EMex <erick00mex@gmail.com>
2018-05-22 20:52:17 -0500

    test

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
 
   url = 'https://www.valorinmuebles.com.mx/landing';
-  // url = 'http://localhost:5000/landing';
+   //url = 'http://localhost:5000/landing';
   
 
   constructor() { }
